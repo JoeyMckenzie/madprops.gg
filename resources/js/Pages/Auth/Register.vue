cbfabbf6728ee610e5e520c22a5d38d0a74b29fa
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-10-05 22:38:03 -0700

    chore: wip

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import InputError from "@/components/InputError.vue";
-import InputLabel from "@/components/InputLabel.vue";
-import PrimaryButton from "@/components/PrimaryButton.vue";
-import TextInput from "@/components/TextInput.vue";
+import Button from "@/components/ui/button/Button.vue";
+import Input from "@/components/ui/input/Input.vue";
+import Label from "@/components/ui/label/Label.vue";
 import GuestLayout from "@/layouts/GuestLayout.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
+import logo from "@svg/logoipsum-332.svg";
 
 const form = useForm({
-    name: "",
+    first_name: "",
+    last_name: "",
     email: "",
     password: "",
     password_confirmation: "",
@@ -15,9 +17,7 @@ const form = useForm({
 
 function submit() {
     form.post(route("register"), {
-        onFinish: () => {
-            form.reset("password", "password_confirmation");
-        },
+        onFinish: () => form.reset("password", "password_confirmation"),
     });
 }
 </script>
@@ -26,89 +26,103 @@ function submit() {
     <GuestLayout>
         <Head title="Register" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Name" />
+        <form
+            class="min-h-screen w-full lg:grid lg:grid-cols-2"
+            @submit="submit"
+        >
+            <div class="flex items-center justify-center px-4 py-12 lg:px-0">
+                <div class="mx-auto grid w-[350px] gap-6">
+                    <img :src="logo" class="mx-auto size-16">
 
-                <TextInput
-                    id="name"
-                    v-model="form.name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                    <div class="grid gap-2 text-center">
+                        <h1 class="text-3xl font-bold">
+                            Register
+                        </h1>
+                        <p class="text-balance text-sm text-muted-foreground">
+                            Enter your name and email below to sign up for an
+                            account
+                        </p>
+                    </div>
+                    <div class="grid gap-4">
+                        <div class="grid grid-cols-2 gap-2">
+                            <div class="grid gap-2">
+                                <Label for="first_name">First name</Label>
+                                <Input
+                                    id="first_name"
+                                    v-model="form.first_name"
+                                    type="text"
+                                    placeholder="Richard"
+                                    required
+                                />
+                                <InputError :message="form.errors.first_name" />
+                            </div>
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                            <div class="grid gap-2">
+                                <Label for="last_name">Last name</Label>
+                                <Input
+                                    id="last_name"
+                                    v-model="form.last_name"
+                                    type="text"
+                                    placeholder="Hendricks"
+                                    required
+                                />
+                                <InputError :message="form.errors.last_name" />
+                            </div>
+                        </div>
+
+                        <div class="grid gap-2">
+                            <Label for="email">Email</Label>
+                            <Input
+                                id="email"
+                                v-model="form.email"
+                                type="email"
+                                placeholder="richard.hendricks@piedpiper.com"
+                                required
+                            />
+                            <InputError :message="form.errors.email" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="password">Password</Label>
+                            <Input
+                                id="password"
+                                v-model="form.password"
+                                type="password"
+                                required
+                            />
+                            <InputError :message="form.errors.password" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="password_confirmation">Confirm password</Label>
+                            <Input
+                                id="password_confirmation"
+                                v-model="form.password_confirmation"
+                                type="password"
+                                required
+                            />
+                            <InputError
+                                :message="form.errors.password_confirmation"
+                            />
+                        </div>
+                        <Button type="submit" class="w-full">
+                            Sign up
+                        </Button>
+                    </div>
+                    <div class="mt-4 text-center text-sm">
+                        Have an account?
+                        <Link :href="route('login')" class="underline">
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel
-                    for="password_confirmation"
-                    value="Confirm Password"
-                />
-
-                <TextInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError
-                    class="mt-2"
-                    :message="form.errors.password_confirmation"
-                />
-            </div>
-
-            <div class="mt-4 flex items-center justify-end">
-                <Link
-                    :href="route('login')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+            <div class="hidden bg-muted lg:block">
+                <img
+                    src="https://www.shadcn-vue.com/placeholder.svg"
+                    alt="Image"
+                    width="1920"
+                    height="1080"
+                    class="size-full object-cover dark:brightness-[0.2] dark:grayscale"
                 >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Register
-                </PrimaryButton>
             </div>
         </form>
     </GuestLayout>
